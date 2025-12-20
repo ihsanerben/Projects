a34dc103c03eba057a8825a9fdc13d93c1ec8893
İhsan Eren Erben <147556034+ihsanerben@users.noreply.github.com>
2025-12-20 15:14:48 +0300

    Add files via upload

---
+++ learningJWT/src/test/java/com/ihsanerben/LearningJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ihsanerben;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
